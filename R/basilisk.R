bbc90529faf54e3c58c9ee34d02f52d504164648
Changqing Wang <wang.ch@wehi.edu.au>
2024-09-25 19:15:35 +1000

    debug GHA with SSH 4

--- R/basilisk.R
+++ R/basilisk.R
@@ -3,17 +3,17 @@ flames_env <- BasiliskEnvironment(
     envname = "flames_env", pkgname = "FLAMES",
     pip = c("fast-edit-distance==1.2.1", "blaze2==2.2.*", "matplotlib==3.9.1"),
     packages = c(
-        "python==3.11.9",
-        "numpy==2.1.1",
-        "scipy==1.14.1",
+        "python==3.8.20",
+        "numpy==1.24.4",
+        "scipy==1.10.1",
         "pysam==0.22.1",
         "cutadapt==4.9",
         "tqdm==4.66.5",
-        "pandas==2.2.3",
+        "pandas==2.0.3",
         "oarfish==0.6.2",
         "minimap2==2.28",
         "samtools==1.21",
-        "k8==0.2.5"
+        "k8==1.2"
     ),
     channels = c("conda-forge", "bioconda")
 )
